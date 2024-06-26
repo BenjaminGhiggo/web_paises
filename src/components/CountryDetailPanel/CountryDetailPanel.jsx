6651e0b4fa01f6e7b738e8bdc83559603123cb93
Ben <victor.ghiggo.d@uni.pe>
2024-06-26 11:49:33 -0500

    mejorando el panel de detalles detail

--- src/components/CountryDetailPanel/CountryDetailPanel.jsx
+++ src/components/CountryDetailPanel/CountryDetailPanel.jsx
@@ -1,25 +1,46 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import defaultImage from '../../assets/default-image.jpg';
+import { getCountryImage } from '../../utils/unsplash';
 import './CountryDetailPanel.scss';
 
 const CountryDetailPanel = ({ country, onClose }) => {
-  console.log('Datos del país para el panel de detalles:', country);
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const image = await getCountryImage(country.name);
+      setImageUrl(image || defaultImage);
+    };
+
+    fetchImage();
+  }, [country.name]);
 
   return (
     <div className="country-detail-panel">
       <button className="close-button" onClick={onClose}>×</button>
       <div className="country-header">
-        <img src={`https://flagpedia.net/data/flags/h80/${country.code.toLowerCase()}.png`} alt={`${country.name} flag`} />
-        <div>
-          <h2>{country.name}</h2>
-          <p>{country.continent.name}</p>
-        </div>
+        <img src={imageUrl} alt={`${country.name}`} className="country-image" />
       </div>
-      <div className="country-info">
-        <p><strong>Capital:</strong> {country.capital}</p>
-        <p><strong>Language:</strong> {country.languages.map(lang => lang.name).join(', ')}</p>
-        <p><strong>Currency:</strong> {country.currency}</p>
-        <p><strong>Region:</strong> {country.awsRegion}</p>
+      <div className="country-content">
+        <div className="country-flag-title">
+          <img 
+            src={`https://flagpedia.net/data/flags/h80/${country.code.toLowerCase()}.png`} 
+            alt={`${country.name} flag`} 
+            className="country-flag"
+          />
+          <div className="country-title">
+            <h2>{country.name}</h2>
+            <p>{country.continent.name}</p>
+          </div>
+        </div>
+        <div className="country-info">
+          <p><strong>Capital:</strong> {country.capital}</p>
+          <p><strong>Language:</strong> {country.languages.map(lang => lang.name).join(', ')}</p>
+          <p><strong>Population:</strong> {country.population}</p>
+          <p><strong>Currency:</strong> {country.currency}</p>
+          <p><strong>Region:</strong> {country.awsRegion}</p>
+        </div>
       </div>
     </div>
   );
@@ -33,6 +54,7 @@ CountryDetailPanel.propTypes = {
     languages: PropTypes.arrayOf(PropTypes.shape({
       name: PropTypes.string.isRequired,
     })).isRequired,
+    population: PropTypes.string,
     currency: PropTypes.string,
     awsRegion: PropTypes.string,
     continent: PropTypes.shape({
